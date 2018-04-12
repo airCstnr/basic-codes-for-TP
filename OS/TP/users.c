24b3f12bc51d12b9ede9d55bd1f1dc38795a77a9
Raf Castanier <raphael.castanier@free.fr>
2018-04-12 10:08:14 +0200

    Modifications du TP4

--- OS/TP/users.c
+++ OS/TP/users.c
@@ -1,21 +1,27 @@
+// Auteurs : Raphaël Castanier, Joffrey Rius Bernal
+// Compilation :  gcc -o prog users.c -Wall -Werror
+
+#include <unistd.h>     // getuid
 #include <stdio.h>      // printf
 #include <pwd.h>        // password
 #include <grp.h>        // groups
-#include <sys/types.h>  // gid_t
+#include <sys/types.h>  // uid_t, gid_t
 
 int main(int argc, char const *argv[]) {
-    // real
+
+    // ID Réel
     uid_t uid = getuid();
     gid_t gid = getgid();
-    // effective
+
+    // ID Effectif
     uid_t euid = geteuid();
     gid_t egid = getegid();
 
-    printf("User     : UID\t\tGID\n");
-    printf("Réel     : %s(UID=%i),\t\t %s(GID=%i)\n",
+    printf("User     : UID\t\t\t\tGID\n");
+    printf("Réel     : %s(UID=%i),\t\t%s(GID=%i)\n",
         getpwuid(uid)->pw_name, uid,
         getgrgid(gid)->gr_name, gid);
-    printf("Effectif : %s(UID=%i),\t\t %s(GID=%i)\n",
+    printf("Effectif : %s(UID=%i),\t\t%s(GID=%i)\n",
         getpwuid(euid)->pw_name, euid,
         getgrgid(egid)->gr_name, egid);
     return 0;
