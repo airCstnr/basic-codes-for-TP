c4381fb5bfb066a8691029c322de40173d65506c
airCstnr <raphael.castanier@free.fr>
2018-05-22 14:03:16 +0200

    OS - TD3 - Exercice 2.2

---
+++ OS/TD/TD3/exercice2-2.c
@@ -0,0 +1,24 @@
+// Auteurs : Raphaël Castanier
+// Compilation :  gcc -o prog exercice2.c
+
+// Ignore les signaux SIGINT et SIGQUIT
+
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+
+void deroutement(int sig)
+{
+    printf("Signal recu : %i\n", sig);
+}
+
+int main(void) {
+    printf("-- PID %i --\n", getpid());
+
+    signal(SIGINT, deroutement);
+
+    while (1) {
+        sleep(1);
+    }
+    return 0;
+}
